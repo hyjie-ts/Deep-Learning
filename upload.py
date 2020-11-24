55695cf437ef1a550e0465cba2558aa364e4509c
“hyjie-ts” <1013210225@qq.com>
2020-11-24 21:03:04 +0800

    “提交项目20.11.24”

---
+++ upload.py
@@ -0,0 +1,24 @@
+
+from qiniu import Auth, put_file, etag, urlsafe_base64_encode
+import datetime
+import time
+bucket_name = 'datat'
+T = str(datetime.date.today())+str(time.strftime("%H:%M:%S"))
+localfile = 'fire_result.avi'
+access_key = 'tNaErVuyTGRTqs6LvtIfCFjZlChw_dD7dTlKTEFR'
+secret_key = 'jdF38Qv3DL6l47Mb13HjiFR4X7746fbZ1lGXLvkS'
+fops = 'avthumb/mp4/s/640x360/vb/1.25m'
+saveas_key = urlsafe_base64_encode(localfile)
+fops = fops+'|saveas/'+saveas_key
+policy={
+  'persistentOps':fops,
+#   'persistentPipeline':pipeline
+ }
+key = 'fire_result'+str(T)+'.avi'
+print(key)
+q = Auth(access_key, secret_key)
+token = q.upload_token(bucket_name, key, 3600,policy)
+ret, info = put_file(token, key, localfile)
+print(info)
+assert ret['key'] == key
+assert ret['hash'] == etag(localfile)
